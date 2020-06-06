184cc3ecdc875a80f4b2ae921749f8f01e581364
guillaumedsde <guillaume@desusanne.com>
2020-06-06 22:32:39 +0200

    ci: don't build armv6

--- build/build.sh
+++ build/build.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/ppc64le \
     -t ${DOCKER_USERNAME}/alpine-qbittorrent-openvpn:${TAG} \
     --push \
     .
