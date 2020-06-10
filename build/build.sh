45b7be5b1fef3a34b4d8e061c14d079cb3dd8a63
guillaumedsde <guillaume@desusanne.com>
2020-06-10 18:16:48 +0200

    ci: swap limit on buildx, squash and compress images

--- build/build.sh
+++ build/build.sh
@@ -2,10 +2,14 @@
 
 AVAILABLE_RAM="$(free -m | awk '/^Mem/ {print $7}')"
 RAM_FOR_BUILD=$((${AVAILABLE_RAM} - ${AVAILABLE_RAM} / 15))
+SWAP_FOR_BUILD=$((${RAM_FOR_BUILD} * 1.5))
 
 docker buildx build \
     --platform=${BUILDX_PLATFORM} \
-    --memory=${RAM_FOR_BUILD}M \
+    --memory="${RAM_FOR_BUILD}m" \
+    --memory-swap="${SWAP_FOR_BUILD}m" \
+    --squash \
+    --compress \
     -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} \
     -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
     --push \
