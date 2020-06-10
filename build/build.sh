2920df916643d89113518a18b9e68a850d20deb6
guillaumedsde <guillaume@desusanne.com>
2020-06-10 17:40:06 +0200

    ci: syntax fix

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,8 @@ RAM_FOR_BUILD=$((${AVAILABLE_RAM} - ${AVAILABLE_RAM} / 15))
 
 docker buildx build \
     --platform=${BUILDX_PLATFORM} \
-    --memory=${RAM_FOR_BUILD}M
--t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} \
+    --memory=${RAM_FOR_BUILD}M \
+    -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} \
     -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
     --push \
     .
