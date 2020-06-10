89f61319e145eb92c19a8a61f5846bade95ffff1
guillaumedsde <guillaume@desusanne.com>
2020-06-10 17:36:16 +0200

    ci: limit RAM usage for docker builx build command

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-docker buildx build --platform=${BUILDX_PLATFORM} -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest --push .
+
+AVAILABLE_RAM="$(free -m | awk '/^Mem/ {print $7}')"
+RAM_FOR_BUILD=$((${AVAILABLE_RAM} - ${AVAILABLE_RAM} / 15))
+
+docker buildx build \
+    --platform=${BUILDX_PLATFORM} \
+    --memory=${RAM_FOR_BUILD}M
+-t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} \
+    -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
+    --push \
+    .
