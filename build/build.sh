0b8ad3d956dad31b3e4b6ebed72cda506fa0f53d
guillaumedsde <guillaume@desusanne.com>
2020-06-10 22:51:34 +0200

    ci: change CI_JOB_STAGE to CI_JOB_NAME

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker buildx build \
-    --platform=${CI_JOB_STAGE} \
+    --platform=${CI_JOB_NAME} \
     --compress \
     -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_SLUG} \
     -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
--- build/prepare_builder.sh
+++ build/prepare_builder.sh
@@ -4,7 +4,7 @@ wget -O $HOME/.docker/cli-plugins/docker-buildx $BUILDX_URL
 chmod a+x $HOME/.docker/cli-plugins/docker-buildx
 "echo -e '{\n  \"experimental\": \"enabled\"\n}' | tee $HOME/.docker/config.json"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use --driver docker-container --name ${BUILDX_BUILDER} --platform=${CI_JOB_STAGE}
+docker buildx create --use --driver docker-container --name ${BUILDX_BUILDER} --platform=${CI_JOB_NAME}
 docker buildx inspect --bootstrap ${BUILDX_BUILDER}
 docker buildx ls
 docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD"
