d1115c39ebabc6b124edd2f9598bba3e7806b3e7
guillaumedsde <guillaume@desusanne.com>
2020-06-12 14:31:50 +0200

    ci: fixed syntax for setting docker parameters

--- build/prepare_builder.sh
+++ build/prepare_builder.sh
@@ -2,7 +2,7 @@
 mkdir -p $HOME/.docker/cli-plugins/
 wget -O $HOME/.docker/cli-plugins/docker-buildx $BUILDX_URL
 chmod a+x $HOME/.docker/cli-plugins/docker-buildx
-"echo -e '{\n  \"experimental\": \"enabled\"\n}' | tee $HOME/.docker/config.json"
+echo -e '{\n  "experimental": "enabled"\n}' | tee $HOME/.docker/config.json
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use --driver docker-container --name ${BUILDX_BUILDER} --platform=${BUILDX_PLATFORM}
 docker buildx inspect --bootstrap ${BUILDX_BUILDER}
