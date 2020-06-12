f4857df15a39a83f1cd51219a39a4bc24c204fe3
guillaumedsde <guillaume@desusanne.com>
2020-06-12 17:30:49 +0200

    fix: double quote build script variables

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 docker buildx build \
-    --platform=${BUILDX_PLATFORM} \
+    --platform="${BUILDX_PLATFORM}" \
     --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VCS_REF=$(git rev-parse --short HEAD) \
     --build-arg VERSION=$(git describe --abbrev=0) \
-    -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
+    -t "${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest" \
     --push \
     .
--- build/prepare_builder.sh
+++ build/prepare_builder.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mkdir -p $HOME/.docker/cli-plugins/
+mkdir -p "$HOME/.docker/cli-plugins/"
 apk add --no-cache git
-wget -O $HOME/.docker/cli-plugins/docker-buildx $BUILDX_URL
-chmod a+x $HOME/.docker/cli-plugins/docker-buildx
-echo -e '{\n  "experimental": "enabled"\n}' | tee $HOME/.docker/config.json
+wget -O "$HOME/.docker/cli-plugins/docker-buildx" "$BUILDX_URL"
+chmod a+x "$HOME/.docker/cli-plugins/docker-buildx"
+echo -e '{\n  "experimental": "enabled"\n}' | tee "$HOME/.docker/config.json"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use --driver docker-container --name ${BUILDX_BUILDER} --platform=${BUILDX_PLATFORM}
-docker buildx inspect --bootstrap ${BUILDX_BUILDER}
+docker buildx create --use --driver docker-container --name "${BUILDX_BUILDER}" --platform="${BUILDX_PLATFORM}"
+docker buildx inspect --bootstrap "${BUILDX_BUILDER}"
 docker buildx ls
 docker logout
 echo "$CI_REGISTRY_PASSWORD" | docker login -u "$CI_REGISTRY_USER" --password-stdin
