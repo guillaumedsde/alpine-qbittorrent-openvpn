66c2d8ae1aeb46461017bdc47046c365a2cfb605
guillaumedsde <guillaume@desusanne.com>
2020-06-12 18:45:18 +0200

    ci: build all branches and more tags to image

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+
+VERSION="$(git describe --abbrev=0)"
+
+if [ "${CI_COMMIT_REF_NAME}" = "master" ]; then
+    TAGS="${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${VERSION} -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest"
+else
+    TAGS="-t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${CI_COMMIT_REF_NAME}"
+fi
+
 docker buildx build \
     --platform="${BUILDX_PLATFORM}" \
     --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-    --build-arg VERSION=$(git describe --abbrev=0) \
-    -t "${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest" \
+    --build-arg VERSION=${VERSION} \
+    ${TAGS} \
     --push \
     .
