4c0a018af6a95211fef7b0119e347bedd538257d
guillaumedsde <guillaume@desusanne.com>
2020-06-12 17:11:28 +0200

    ci: fix build script

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,7 @@ docker buildx build \
     --platform=${BUILDX_PLATFORM} \
     --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-    --build-arg VERSION=$(git describe --abbrev=0)
--t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest --push .
+    --build-arg VERSION=$(git describe --abbrev=0) \
+    -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest \
+    --push \
+    .
