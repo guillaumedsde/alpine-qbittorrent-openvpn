c824bd8bde566ff6bdac451f6c5924186baf9604
guillaumedsde <guillaume@desusanne.com>
2020-06-15 14:55:58 +0200

    fix: double quote variables to prevent word splitting

--- build/build.sh
+++ build/build.sh
@@ -15,7 +15,7 @@ docker buildx build \
     --platform="${BUILDX_PLATFORM}" \
     --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-    --build-arg VERSION=${VERSION} \
-    ${TAGS} \
+    --build-arg VERSION="${VERSION}" \
+    "${TAGS}" \
     --push \
     .
