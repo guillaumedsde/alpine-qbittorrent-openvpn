58a239fbcb371f1d7784f60b8d01b56effb011ca
guillaumedsde <guillaume@desusanne.com>
2020-06-18 21:54:08 +0200

    ci: fix build script

--- build/build.sh
+++ build/build.sh
@@ -11,11 +11,10 @@ else
     TAGS="-t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${BRANCH}"
 fi
 
-docker buildx build \
+docker buildx build . \
     --platform="${BUILDX_PLATFORM}" \
     --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
     --build-arg VCS_REF=$(git rev-parse --short HEAD) \
     --build-arg VERSION="${VERSION}" \
-    "${TAGS}" \
-    --push \
-    .
+    ${TAGS} \
+    --push
