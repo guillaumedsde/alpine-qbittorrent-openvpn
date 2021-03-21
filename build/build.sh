28c95a5726dc602a53652abfc073186e06fe7c5c
guillaumedsde <guillaume@desusanne.com>
2021-03-21 18:36:54 +0100

    feat: use python based image everywhere

--- build/build.sh
+++ build/build.sh
@@ -2,16 +2,7 @@
 
 if [ "${CI_COMMIT_REF_NAME}" = "master" ]; then
     VERSION="$(git rev-parse --short HEAD)"
-    # build python version
-    docker buildx build . \
-        --platform="${BUILDX_PLATFORM}" \
-        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-        --build-arg VCS_REF="$(git rev-parse --short HEAD)" \
-        --build-arg VERSION="python" \
-        --build-arg BASE_IMAGE="python:3-alpine3.12" \
-        -t "${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:python" \
-        --push
-    TAGS=" -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${VERSION} -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest "
+    TAGS="-t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:${VERSION} -t ${CI_REGISTRY_USER}/alpine-qbittorrent-openvpn:latest"
 else
     # cleanup branch name
     BRANCH="$(echo "${CI_COMMIT_REF_NAME}" | tr / _)"
