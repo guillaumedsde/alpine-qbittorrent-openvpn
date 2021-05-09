8a650fc6841a2c4e97ea4b2d1ab6ba41f9dc19ee
guillaumedsde <guillaume@desusanne.com>
2021-05-09 17:08:34 +0200

    feat: use userdocs build script and upgrade to qbittorrent 4.3.5

--- rootfs/usr/sbin/install_qbittorrent.sh
+++ rootfs/usr/sbin/install_qbittorrent.sh
@@ -3,19 +3,17 @@ set -x
 ARCH="$(uname -m)"
 echo "building for ${ARCH}"
 
-QBT_VERSION=4.3.4.1
-QBT_RELEASE=r1
+QBITTORRENT_VERSION=4.3.5
+LIBTORRENT_VERSION=v2.0.3
 
 if [ "${ARCH}" = "x86_64" ]; then
-    qbit_arch="amd64"
+    qbit_arch="x86_64"
 elif echo "${ARCH}" | grep -E -q "aarch64_be|aarch64|armv8b|armv8l|arm64"; then
-    qbit_arch="arm64"
-elif echo "${ARCH}" | grep -E -q "s390|s390x"; then
-    qbit_arch="s390x"
+    qbit_arch="aarch64"
 fi
 
 if [ -n "$qbit_arch" ]; then
-    wget "https://github.com/guillaumedsde/qbittorrent-nox-static/releases/download/${QBT_VERSION}-${QBT_RELEASE}/qbittorrent-nox-v${QBT_VERSION}-static-${qbit_arch}" -O /usr/bin/qbittorrent-nox
+    wget "https://github.com/userdocs/qbittorrent-nox-static/releases/download/release-${QBITTORRENT_VERSION}_${LIBTORRENT_VERSION}/qbittorrent-nox-${qbit_arch}" -O /usr/bin/qbittorrent-nox
     chmod +x /usr/bin/qbittorrent-nox
 else
     apk add --no-cache -X "http://dl-cdn.alpinelinux.org/alpine/edge/testing" qbittorrent-nox
