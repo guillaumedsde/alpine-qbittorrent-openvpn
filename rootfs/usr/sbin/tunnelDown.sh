64f5011d0729056360b08f68588e6cb90f62de32
jaype87 <jaype87@gmail.com>
2021-12-17 20:18:09 +0100

    fix usage of env variable

--- rootfs/usr/sbin/tunnelDown.sh
+++ rootfs/usr/sbin/tunnelDown.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -x /${SCRIPTS_DIR}tunnelDown.sh ]]; then
-  echo "Executing /${SCRIPTS_DIR}tunnelDown.sh"
-  /${SCRIPTS_DIR}tunnelDown.sh 
-  echo "/${SCRIPTS_DIR}tunnelDown.sh returned $?"
+if [[ -x ${SCRIPTS_DIR}/tunnelDown.sh ]]; then
+  echo "Executing ${SCRIPTS_DIR}/tunnelDown.sh"
+  ${SCRIPTS_DIR}/tunnelDown.sh 
+  echo "${SCRIPTS_DIR}/tunnelDown.sh returned $?"
 else
   echo "${SCRIPTS_DIR}/tunnelDown.sh does not exist."
 fi
