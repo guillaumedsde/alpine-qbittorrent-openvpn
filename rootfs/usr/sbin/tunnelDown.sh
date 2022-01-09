3db74089ada1774409ef23580418b76ac04f8a2a
jaype87 <jaype87@users.noreply.github.com>
2022-01-10 00:07:46 +0100

    Update rootfs/usr/sbin/tunnelDown.sh
    Co-authored-by: guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>

--- rootfs/usr/sbin/tunnelDown.sh
+++ rootfs/usr/sbin/tunnelDown.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -x /scripts/tunnelDown.sh ]]; then
-  echo "Executing /scripts/tunnelDown.sh"
+  echo "INFO: Executing /scripts/tunnelDown.sh"
   /scripts/tunnelDown.sh 
-  echo "/scripts/tunnelDown.sh returned $?"
+  echo "INFO: /scripts/tunnelDown.sh returned $?"
 else
-  echo "/scripts/tunnelDown.sh does not exist."
+  echo "INFO: /scripts/tunnelDown.sh does not exist."
 fi
