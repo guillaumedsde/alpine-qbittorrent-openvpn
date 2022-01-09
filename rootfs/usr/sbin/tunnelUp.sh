60606d3a9da96c7e7e5bcada8dc0310eedae25f9
jaype87 <jaype87@users.noreply.github.com>
2022-01-10 00:08:03 +0100

    Update rootfs/usr/sbin/tunnelUp.sh
    Co-authored-by: guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>

--- rootfs/usr/sbin/tunnelUp.sh
+++ rootfs/usr/sbin/tunnelUp.sh
@@ -3,9 +3,9 @@
 echo "INFO: VPN connection is UP"
 
 if [[ -x /scripts/tunnelUp.sh ]]; then
-  echo "Executing scripts/tunnelUp.sh"
+  echo "INFO: Executing scripts/tunnelUp.sh"
   /scripts/tunnelUp.sh 
-  echo "/scripts/tunnelUp.sh returned $?"
+  echo "INFO: /scripts/tunnelUp.sh returned $?"
 else
-  echo "/scripts/tunnelUp.sh does not exist."
+  echo "INFO: /scripts/tunnelUp.sh does not exist."
 fi
